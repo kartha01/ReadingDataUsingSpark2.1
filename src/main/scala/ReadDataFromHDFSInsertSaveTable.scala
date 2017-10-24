1a31f9dd1f346d3f44f645e4020804e838ddd013
root <root@myweb1.fyre.ibm.com>
2017-10-24 11:41:00 -0700

    Updates to file

--- src/main/scala/ReadDataFromHDFSInsertSaveTable.scala
+++ src/main/scala/ReadDataFromHDFSInsertSaveTable.scala
@@ -12,3 +12,12 @@ df1.show()
 //save table to query via Hive
 df1.write.mode("overwrite").saveAsTable("default.A1")
 
+//continue using the old temp table a1
+df1.registerTempTable("a2")
+
+//No support for UPDATE
+//scala> val df2=spark.sql("Update a2 set _c1='tx' where _c0=3")
+//org.apache.spark.sql.catalyst.parser.ParseException: {'(', 'SELECT', 'FROM', 'ADD', 'DESC', 'WITH', 'VALUES', 'CREATE', 'TABLE', 'INSERT', 'DELETE', 'DESCRIBE', 'EXPLAIN', 'SHOW', 'USE', 'DROP', 'ALTER', 'MAP', 'SET', 'RESET', 'START', 'COMMIT', 'ROLLBACK', 'REDUCE', 'REFRESH', 'CLEAR', 'CACHE', 'UNCACHE', 'DFS', 'TRUNCATE', 'ANALYZE', 'LIST', 'REVOKE', 'GRANT', 'LOCK', 'UNLOCK', 'MSCK', 'EXPORT', 'IMPORT', 'LOAD'}(line 1, pos 0)
+
+
+
